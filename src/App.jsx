3df880b4640a80a2c2d7d6da0569bf4750083153
nitidoart <113176158+nitidoart@users.noreply.github.com>
2024-04-30 11:07:52 +0200

    modified (*.jsx):implementyacion de rutas privadas

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,7 @@ import Home from './views/home/Home';
 import Detail from './views/detail/Detail';
 import Form from './views/create/form/Form';
 import NotFound from './utils/notFound/NotFound';
-import {ProtectedRoute} from './views/ProtectedRoute/ProtectedRoute'
+import { PrivateRoute } from './views/PrivateRoute/PrivateRoute'
 
 function App() {
   return (
@@ -12,14 +12,15 @@ function App() {
 
       <Routes>
         <Route exact path="/" element={<Landing />} />
-        {/*  <Route path="/home" element={<Home/>}/> */}
-       
-        <Route path="/" element={<ProtectedRoute />}>
+        {/* <Route path="/home" element={<Home/>}/> */}
+
+        <Route path="/" element={<PrivateRoute />}>
           <Route path="home" element={<Home />} />
+          <Route path="/home/:id" element={<Detail />} /> //Envio por params el ID
+          <Route path="/create" element={<Form />} />
         </Route>
 
-        <Route path="/home/:id" element={<Detail />} /> //Envio por params el ID
-        <Route path="/create" element={<Form />} />
+
 
         {/* Ruta para manejar rutas no definidas */}
         <Route path="*" element={<NotFound />} />
--- src/components/Login/Login.jsx
+++ src/components/Login/Login.jsx
@@ -1,33 +1,67 @@
 import React, { useState } from 'react'
+import { useForm } from './useForm';
+import { useNavigate } from 'react-router-dom';
+
 
 
 const Login = () => {
-    const [userName, setUserName] = useState('');
-    const [password, setPassword] = useState('');
+    /* const [userName, setUserName] = useState('');
+    const [password, setPassword] = useState(''); */
+
+    const navigate = useNavigate();
+
+    const { name, password, onInputChange, onResetForm } = useForm({
+        name: '',
+        password: '',
+    });
+
+    /* const handleSubmit = (event) => {
+        event.preventDefault()
+        console.log({
+            name: userName,
+            password: password
+        });
+    } */
 
     const handleSubmit = (event) => {
         event.preventDefault()
-        console.log('Submit login');
+        navigate('/home', {
+            repalce: true,
+            state:{
+                logged:true, /* propiedad personalixada */
+                name,
+            },
+        });
+        onResetForm();
     }
 
     return (
-        <div>
+        <div className='wrapper'>
             <form action="" onSubmit={handleSubmit}>
                 <input
                     type="text"
-                    value={userName}
-                    name='Username'
-                    placeholder='Username'
-                    onChange={({ target }) => setUserName(target.value)}
+                    value={name}
+                    name='name'
+                    id='name'
+                    placeholder='name'
+                    /* onChange={({ target }) => setUserName(target.value)} */
+                    onChange={onInputChange}
+                    required
+                    autoComplete='off'
                 />
                 <input
                     type="password"
                     value={password}
-                    name='Password'
-                    placeholder='Password'
-                    onChange={({ target }) => setPassword(target.value)}
+                    name='password'
+                    id='password'
+                    placeholder='password'
+                    /* onChange={({ target }) => setPassword(target.value)} */
+                    onChange={onInputChange}
+                    required
+                    autoComplete='off'
                 />
                 <button>Login</button>
+                <p>Iniciar sesión</p>
             </form>
         </div>
     )
